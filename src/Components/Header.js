d230169e9898e01999ae8316a47696f312b1f160
Ryan <ryancastles33@gmail.com>
2021-03-02 20:01:48 +1100

    Created react app and published to github pages

---
+++ src/Components/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var resumeDownload = this.props.data.resumedownload;
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <header id="home">
+
+      <nav id="nav-wrap">
+
+         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" className="nav">
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            <li><a className="smoothscroll" href="#about">About</a></li>
+            <li><a className="smoothscroll" href="#resume">Resume</a></li>
+            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+         </ul>
+
+      </nav>
+
+      <div className="row banner">
+         <div className="banner-text">
+            <h1 className="responsive-headline">{name}</h1>
+            <hr />
+            <ul className="social">
+               {networks}
+            </ul>
+         </div>
+         <div>
+         <p>
+            <a href={resumeDownload} target="_blank" download className="button"><i className="fa fa-download"></i>Download Resume</a>
+         </p>
+      </div>
+      </div>
+
+      <p className="scrolldown">
+         <a className="smoothscroll" href="#resume"><i className="icon-down-circle"></i></a>
+      </p>
+
+   </header>
+    );
+  }
+}
+
+export default Header;
