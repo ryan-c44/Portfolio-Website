2df0432fe849f5c2dbfba8be193e57acf265eaa6
ryan-c44 <ryancastles33@gmail.com>
2021-09-01 19:48:49 +1000

    Made some resume updates
    updated skills and technologies section along with extracurricular activties.

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,68 +6,98 @@ class Resume extends Component {
     if(this.props.data){
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        <p className="info"><em>{education.degree}</em> <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <p>{education.description}</p><p><a href={education.link} download>Academic Transcript</a></p></div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+            <p className="info"><em>{work.title}</em><span></span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+        
+        return <li key={skills.name}><em>{skills.name}</em></li>
+      })
+      var technologies = this.props.data.technologies.map(function(technologies){
+        
+        return <li key={technologies.name}><em>{technologies.name}</em></li>
+      });
+      var extracurricular = this.props.data.extracurricular.map(function(extracurricular){
+        return <div key={extracurricular.name}><h3>{extracurricular.name}</h3>
+            
+            <p>{extracurricular.desc}</p>
+        </div>
       })
     }
 
     return (
       <section id="resume">
 
-      <div className="row education">
-         <div className="three columns header-col">
+        <div className="row education">
+          <div className="three columns header-col">
             <h1><span>Education</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">
+                {education}
+              </div>
             </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work / Experience</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+          </div>
         </div>
-    </div>
+
+
+        <div className="row work">
+
+          <div className="three columns header-col">
+            <h1><span>Work / Experience</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {work}
+          </div>
+        </div>
 
 
 
-      <div className="row skill">
+        <div className="row skill">
 
-         <div className="three columns header-col">
+          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
 
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+            <div>
+              <div className="bars">
+
+                <ul>
+                  {skills}
+                </ul>
+              </div>
+              <div className="bars-right">
+                <ul>
+                  {technologies}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div className="row skill">
+
+        <div className="three columns header-col">
+            <h1><span>Extracurricular</span></h1>
+          </div>
+          <div className="nine columns main-col">
+            {extracurricular}
+          </div>
+
+        </div>
+      </section>
     );
   }
 }
